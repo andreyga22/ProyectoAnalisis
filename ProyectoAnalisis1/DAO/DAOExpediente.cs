218c9e75b16bd847f328c409613ccc0eb4550db1
Andrey Gonzalez Artaviaasdasdaasdf <andreyga22@outlook.com>
2018-10-06 16:55:58 -0600

    creado DaoExpediente

---
+++ ProyectoAnalisis1/DAO/DAOExpediente.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TO;
+using System.Data.SqlClient;
+namespace DAO
+{
+    public class DAOExpediente
+    {
+        SqlConnection conexion = new SqlConnection(Properties.Settings.Default.conection);
+        public void insertar(TOExpediente to) {
+
+        }
+
+        public void pruebaConexion() {
+            SqlCommand comando = new SqlCommand("insert into Direccion values () ");
+        }
+    }
+}
