4bcb494d65438a0bbb1b6b6cd1d336c39e31cf4f
Steven Aguilar <timosnes@gmail.com>
2018-10-10 20:40:11 -0600

    DAO fichaDoctor y ficha paramedico

---
+++ ProyectoAnalisis1/DAO/DAOFichaDoctor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TO;
+
+namespace DAO
+{
+    class DAOFichaDoctor
+    {
+        SqlConnection conexion = new SqlConnection(Properties.Settings.Default.conection);
+
+        public void insertar(TOFichaDoctor ficha)
+        {
+            string insert = "insert into FICHA_PARAMEDICO (motivo_consulta, " +
+                "examen_fisico, plan) values (@motivo, @examen, @plan)";
+
+            SqlCommand insertar = new SqlCommand(insert, conexion);
+            insertar.Parameters.AddWithValue("@motivo", ficha.motivoConsulta);
+            insertar.Parameters.AddWithValue("@examen", ficha.examenFisico);
+            insertar.Parameters.AddWithValue("@plan", ficha.plan);
+
+            if (conexion.State != System.Data.ConnectionState.Open)
+            {
+                conexion.Open();
+            }
+
+            insertar.ExecuteNonQuery();
+
+            if (conexion.State != System.Data.ConnectionState.Closed)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
---
+++ ProyectoAnalisis1/DAO/DAOFichaParamedico.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using TO;
+
+namespace DAO
+{
+    class DAOFichaParamedico
+    {
+        SqlConnection conexion = new SqlConnection(Properties.Settings.Default.conection);
+
+        public void insertar(TOFichaParamedico ficha)
+        {
+            string insert = "insert into FICHA_PARAMEDICO (presion_arterial, " +
+                "temperatura, estatura, peso, niveles_azucar, oximetria_pulso, observaciones) " +
+                "values (@presion, @temperatura, @estatura, @peso, @niveles_azucar, @oximetria_pulso, " +
+                "@observaciones)";
+
+            SqlCommand insertar = new SqlCommand(insert, conexion);
+            insertar.Parameters.AddWithValue("@presion", ficha.presionArterial);
+            insertar.Parameters.AddWithValue("@temperatura", ficha.temperatura);
+            insertar.Parameters.AddWithValue("@estatura", ficha.estatura);
+            insertar.Parameters.AddWithValue("@peso", ficha.peso);
+            insertar.Parameters.AddWithValue("@niveles_azucar", ficha.nivel_Azucar);
+            insertar.Parameters.AddWithValue("@oximetria_pulso", ficha.oximetria_Pulso);
+            insertar.Parameters.AddWithValue("@observaciones", ficha.observacion);
+
+            if (conexion.State != System.Data.ConnectionState.Open)
+            {
+                conexion.Open();
+            }
+
+            insertar.ExecuteNonQuery();
+
+            if (conexion.State != System.Data.ConnectionState.Closed)
+            {
+                conexion.Close();
+            }
+        }
+    }
+}
