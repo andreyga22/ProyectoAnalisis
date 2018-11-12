b524b60e3e0dcecef6dc5fa89219b3809db570f3
Steven Aguilar <timosnes@gmail.com>
2018-11-12 00:45:20 -0600

    pruebas

---
+++ ProyectoAnalisis1/pruebasProyectoTasiste/pruebaManejadorDireccion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BL;
+
+namespace pruebasProyectoTasiste
+{
+    [TestClass]
+    public class pruebaManejadorDireccion
+    {
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void pruebaInsertarDireccion_ThrowsNull()
+        {
+            //Arrange
+
+            BLDireccion dir_null = null;
+            BLManejadorDireccion manejador_prueba = new BLManejadorDireccion();
+
+            //Act
+            manejador_prueba.insertar(dir_null);
+        }
+
+        [TestMethod]
+        public void pruebaInsertarDireccion()
+        {
+            //Arrange
+
+            BLDireccion dir_prueba = new BLDireccion(19, "32165", "Alajuela", "naranjo", "cirri", "100 m suer de la plaza de deportes");
+            BLManejadorDireccion manejador_prueba = new BLManejadorDireccion();
+
+
+            //Act
+            manejador_prueba.insertar(dir_prueba);
+
+            //Assert
+
+            Assert.AreEqual(dir_prueba.idDireccion, (manejador_prueba.consultar(dir_prueba.cedula).idDireccion));
+        }
+    }
+}
