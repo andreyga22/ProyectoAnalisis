70d7fbb610cf073f217c18ea237abff69dcfecd4
Steven Aguilar <timosnes@gmail.com>
2018-12-02 23:01:43 -0600

    Pruebas empleado y expediente

---
+++ ProyectoAnalisis1/pruebasProyectoTasiste/pruebaManejadorEmpleado.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BL;
+using System.Data.SqlClient;
+
+namespace pruebasProyectoTasiste
+{
+    /// <summary>
+    /// Summary description for pruebaManejadorEmpleadocs
+    /// </summary>
+    [TestClass]
+    public class pruebaManejadorEmpleado
+    {
+        [TestClass]
+        public class pruebaManejadorDireccion
+        {
+            [TestMethod]
+            [ExpectedException(typeof(NullReferenceException))]
+            public void pruebaInsertarEmpleado_ThrowsNull()
+            {
+
+                BLEmpleado emp_null = null;
+                BLManejadorEmpleado manejador = new BLManejadorEmpleado();
+
+                manejador.crearNuevoEmpleado(emp_null);
+            }
+
+            [TestMethod]
+            [ExpectedException(typeof(SqlException))]
+            public void pruebaInsertarEmpleado_SqlException()
+            {
+
+                BLEmpleado emp_prueba = new BLEmpleado("Amirul", "amiru44", "Armando Fernandez", "Doctooooooooooooooooooooooooooooooooooooooor", true);
+                BLManejadorEmpleado manejador_prueba = new BLManejadorEmpleado();
+                manejador_prueba.crearNuevoEmpleado(emp_prueba);
+            }
+
+            [TestMethod]
+            public void pruebaInsertarEmpleado()
+            {
+                BLEmpleado emp_prueba = new BLEmpleado("Hernan", "Feris12", "Doctor", "Fabio Lopez", true);
+                BLManejadorEmpleado manejador_prueba = new BLManejadorEmpleado();
+                manejador_prueba.crearActualizarEmpleado(emp_prueba);
+                Assert.AreEqual(emp_prueba.id, manejador_prueba.buscarEmpleado(emp_prueba.id, emp_prueba.contrasenna).id);
+       
+                
+
+            }
+        }
+    }
+}
--- ProyectoAnalisis1/pruebasProyectoTasiste/pruebaManejadorExpediente.cs
+++ ProyectoAnalisis1/pruebasProyectoTasiste/pruebaManejadorExpediente.cs
@@ -10,37 +10,37 @@ namespace pruebasProyectoTasiste
     [TestClass]
     public class pruebaManejadorExpediente
     {
-        //[TestMethod]
-        //[ExpectedException(typeof(NullReferenceException))]
-        //public void pruebaInsertarModificarExpediente_ThrowsNull()
-        //{
-        //    BLExpediente exped = null;
-        //    BLManejadorExpediente manejExp = new BLManejadorExpediente();
+        [TestMethod]
+        [ExpectedException(typeof(NullReferenceException))]
+        public void pruebaInsertarModificarExpediente_ThrowsNull()
+        {
+            BLExpediente exped = null;
+            BLManejadorExpediente manejExp = new BLManejadorExpediente();
 
-        //    manejExp.insertarModificar(exped);
-        //}
+           manejExp.insertarModificar(exped);
+        }
 
-        //[TestMethod]
-        //[ExpectedException(typeof(SqlException))]
-        //public void pruebaInsertarModificarExpediente_ThrowsSqlException()
-        //{
-        //    //BLExpediente exped = new BLExpediente("123", "Maya", "María", "Sol", "Roj", Convert.ToDateTime("2016/11/28"), "89782156", "N/A", "Soltera", "Estudiante", "Femenino");
-        //    BLManejadorExpediente manejExp = new BLManejadorExpediente();
+        [TestMethod]
+        [ExpectedException(typeof(SqlException))]
+        public void pruebaInsertarModificarExpediente_ThrowsSqlException()
+        {
+            BLExpediente exped = new BLExpediente("12643", "Miriam", "Maria", "Sol", "Roj", Convert.ToDateTime("2016/11/28"), "89782156", "N/A", "Solteraaaaaaaaaaaaaaa", "Estudiante", "Femenino", "89788156", "80782156", "telefono de mi madre", "telefono de mi padre");
+            BLManejadorExpediente manejExp = new BLManejadorExpediente();
 
-        //    manejExp.insertarModificar(exped);
+            manejExp.insertarModificar(exped);
 
-        //}
+        }
 
 
-        //[TestMethod]
-        //public void pruebaInsertarModificarExpediente()
-        //{
-        //    //BLExpediente exped = new BLExpediente("123", "Maya", "María", "Sol", "Roj", Convert.ToDateTime("2016/11/28"), "89782156", "N/A", "Soltera", "Estudiante", "F");
-        //    BLManejadorExpediente manejExp = new BLManejadorExpediente();
+        [TestMethod]
+        public void pruebaInsertarModificarExpediente()
+        {
+            BLExpediente exped = new BLExpediente("45789", "Fabiola", "Maria", "Sol", "Roj", Convert.ToDateTime("2016/11/28"), "89782056", "N/A", "Soltera", "Estudiante", "F", "89788156", "80782156", "telefono de mi madre", "telefono de mi padre");
+            BLManejadorExpediente manejExp = new BLManejadorExpediente();
 
-        //    manejExp.insertarModificar(exped);
+            manejExp.insertarModificar(exped);
 
-        //    Assert.AreEqual(exped.cedula, (manejExp.consultarExpediente(exped.cedula).cedula));
-        //}
+           Assert.AreEqual(exped.cedula, (manejExp.consultarExpediente(exped.cedula).cedula));
+        }
     }
 }
